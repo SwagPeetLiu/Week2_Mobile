b24385e6cdd4943fbf9e6e575f3b7e65ba4542f3
SwagPeetLiu <111214651+SwagPeetLiu@users.noreply.github.com>
2023-07-31 14:29:08 +1000

    Your commit message here

---
+++ app/src/main/java/com/example/firstdemo/MainActivity2_Test.java
@@ -0,0 +1,14 @@
+package com.example.firstdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2_Test extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity2_test);
+    }
+}
